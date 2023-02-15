03bd150e797a0b8e9eb1303ac8507d7fdeb00f98
Earle Fox <earle.fox312@gmail.com>
2023-02-15 13:00:26 -0500

    first commit

---
+++ SearchingOMDB/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SearchingOMDB.Models;
+using System.Diagnostics;
+
+namespace SearchingOMDB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieSearchForm()
+        {
+            return View("MovieSearch");
+        }
+        [HttpPost]
+        public IActionResult MovieSearchResult(string title)
+        {
+            return View("MovieSearch",SingleMovieDAL.MovieSearch(title)) ;
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SearchingOMDB/Models/SingleMovieDAL.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace SearchingOMDB.Models
+{
+    public class SingleMovieDAL
+    {
+        public static SingleMovie MovieSearch(string input)//adjust here
+        {
+            //Adjust here
+            //Setup
+            string url = $"https://www.omdbapi.com/?t={input}&apikey={Secret.ApiKey}";
+
+            //request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Convert it to JSON
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Adjust here
+            //Convert to C#
+            SingleMovie result = JsonConvert.DeserializeObject<SingleMovie>(JSON);
+            return result;
+        }
+    }
+}
